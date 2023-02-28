b8a9929107ae65de056eeff0e7442d5b98e17a8c
Winston Oudshoorn <woudshoorn00@hotmail.com>
2023-02-28 13:17:06 +0100

    Added pytest-cov and covered all relevant functions in models.py

--- test_models.py
+++ test_models.py
@@ -13,6 +13,16 @@ def test_daily_mean_zeros():
     test_result = np.zeros(2)
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_min_integers():
+    test_input = np.array([[1,7], [3,4], [5,6]])
+    test_result = [1, 4]
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_max_integers():
+    test_input = np.array([[1,2], [3,4], [2,6]])
+    test_result = [3, 6]
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     
