5a4d38bbef47092693127a892669b8ff91ac72ca
Winston Oudshoorn <woudshoorn00@hotmail.com>
2023-02-28 13:04:29 +0100

    imported pytest

--- test_models.py
+++ test_models.py
@@ -19,6 +19,8 @@ def test_daily_min_string():
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
-test_daily_mean_integers()
-test_daily_mean_zeros()
-test_daily_min_string()
+
+@pytest.mark.parametrize("test_input, test_result", [ ([[1,2], [3,4], [5,6]]), ([3, 4]) ]), ([np.zeros((3, 2))], ([np.zeros(2)]) ])
+
+def test_daily_mean(test_input, test_result):
+    npt.assert_array_equal(daily_mean(test_input), test_result)
